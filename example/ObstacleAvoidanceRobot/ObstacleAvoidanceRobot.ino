db6f7f84cb272ca74f23360cf133d7b618d0433b
Raghunandan B <mbrraghu@gmail.com>
2023-08-29 16:33:44 +0530

    Add Obstacle avoidance robot example.

---
+++ example/ObstacleAvoidanceRobot/ObstacleAvoidanceRobot.ino
@@ -0,0 +1,20 @@
+#include "TwoWDRobot.h"
+#define TRIG 2 //Arduino pin connected to HC-SR04 trigger pin.
+#define ECHO 3 //Arduino pin connected to HC-SR04 echo pin.
+
+//Object to access the methods to run obstacle avoidance robot.
+//It takes inputs for ultrasound control pins and MDD3A motor driver pins.
+obstacleAvoidanceRobot myRobot(TRIG,ECHO,5,6,9,10,PWM_PWM);
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.print("Sensor Distance: ");Serial.println(myRobot.getUltrasoundDistance());
+  //Method takes minimum obstacle distance to avoid and delay time to deviate from the obstacle.
+  //By default PWM values for motors are set to 50 each, user can change as per their needs.
+  myRobot.runTheRobot(30,500);
+}
