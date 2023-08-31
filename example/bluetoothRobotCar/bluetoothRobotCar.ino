adb21f935d898145bb7e124bdee683c39d1980bb
Raghunandan B <mbrraghu@gmail.com>
2023-08-31 20:25:55 +0530

    Update the comments on the bluetooth example.

--- example/bluetoothRobotCar/bluetoothRobotCar.ino
+++ example/bluetoothRobotCar/bluetoothRobotCar.ino
@@ -11,6 +11,9 @@ void setup()
 
 void loop()
 {
-  myRobot.runRobotCar(70, 70);
+  // Method to run the bluetooth controlled robot.
+  // both the motor speeds are set to 50 PWM values by default. User can key in the required speeds.
+  myRobot.runRobotCar();
+  // Method to print the data received by the bluetooth.
   Serial.println(myRobot.bluetoothData());
 }
