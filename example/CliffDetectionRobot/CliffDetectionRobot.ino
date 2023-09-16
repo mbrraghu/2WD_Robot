281619f214cca868fb393be81727a7e81300a5d9
Raghunandan B <mbrraghu@gmail.com>
2023-09-16 22:26:33 +0530

    Update Cliff detection Robot example

--- example/CliffDetectionRobot/CliffDetectionRobot.ino
+++ example/CliffDetectionRobot/CliffDetectionRobot.ino
@@ -1,10 +1,10 @@
 #include "TwoWDRobot.h"
-#define LEFT_IR 7 // Arduino pin connected to HC-SR04 trigger pin.
-#define RIGHT_IR 8 // Arduino pin connected to HC-SR04 echo pin.
+#define LEFT_IR 2 // Arduino pin connected to left IR signal pin.
+#define RIGHT_IR 3 // Arduino pin connected to right IR signal pin.
 
 // Object to access the methods to run cliff detection robot.
 // It takes inputs for two IR sensors pins and MDD3A motor driver pins.
-cliffDetectionRobot myRobot(LEFT_IR, RIGHT_IR,5,9,10,11, PWM_PWM);
+cliffDetectionRobot myRobot(LEFT_IR, RIGHT_IR,5,6,9,10, PWM_PWM);
 
 void setup()
 {
@@ -16,5 +16,5 @@ void loop()
   // Method takes delay time to deviate from the cliff and sensor type.
   // if sensors are active high, arguments will be 'true' by default else it should be 'false'.
   // By default PWM values for motors are set to 50 each, user can change as per their needs.
-  myRobot.runCliffRobot(ACTIVE_LOW, 500);
+  myRobot.runCliffRobot(ACTIVE_LOW, 1500,30,30);
 }
